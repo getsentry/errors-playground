4308ba016e9ec4551f25529378abaf833034c395
Lazar Nikolov <hello@nikolovlazar.com>
2023-06-28 14:46:48 -0400

    defining new layout

---
+++ packages/docsite/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Rubik Variable", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        background: {
+          light: "#F6F6F8",
+        },
+        "rich-black": "#181225",
+        violet: "#584774",
+        "violet-light": "#79628C",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
